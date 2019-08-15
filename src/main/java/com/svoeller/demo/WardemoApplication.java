3fd3d1f7421421cdbd20b3522ef87428288ccf9c
Sean Voeller <sean.voeller@gmail.com>
2019-08-15 09:19:50 -0700

    initial

---
+++ src/main/java/com/svoeller/demo/WardemoApplication.java
@@ -0,0 +1,13 @@
+package com.svoeller.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardemoApplication.class, args);
+	}
+
+}
